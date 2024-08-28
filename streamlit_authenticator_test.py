1ca88a409c124215b8e3e3301bf9c8d5c59f5bef
Zhan Wu <64136291+Ethan8888@users.noreply.github.com>
2024-08-28 11:06:14 +0800

    Update streamlit_authenticator_test.py

--- streamlit_authenticator_test.py
+++ streamlit_authenticator_test.py
@@ -1,5 +1,6 @@
 import yaml
 import streamlit as st
+import io
 import streamlit_authenticator as stauth
 from streamlit_authenticator.utilities.exceptions import (CredentialsError,
                                                           ForgotError,
@@ -9,7 +10,7 @@ from streamlit_authenticator.utilities.exceptions import (CredentialsError,
                                                           UpdateError) 
 
 # Loading config file
-with open('../config.yaml', 'r', encoding='utf-8') as file:
+with io.open('../config.yaml', 'r', encoding='utf-8') as file:
     config = yaml.load(file)
 
 # Creating the authenticator object
